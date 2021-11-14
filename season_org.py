093e2a99b3ed0073e8f76fcb00b562aab6837cf3
Amen Wondwosen <amenwond408@gmail.com>
2021-11-14 15:38:13 -0800

    added function to sort metadata files by season

--- season_org.py
+++ season_org.py
@@ -2,20 +2,33 @@ import json
 import os
 from pathlib import Path
 
-db_dir = Path("./db/mal_files")
+DEFAULT_DB_DIR = Path("./db/mal_files")
+DEFAULT_SEASON_DIR = Path("./db/seasons")
 
 # TODO: create a txt that contains all categorized anime
 
-def run():
+def sort_seasons(db_dir=DEFAULT_DB_DIR, season_dir=DEFAULT_SEASON_DIR):
     for anime in db_dir.iterdir():
         print(anime.name)
         
         # get the metadata from the file
         with anime.open(encoding="utf-8") as infile:
             metadata = json.loads(infile.read())
-        print(metadata["season"])
+
+        # Form a filepath based on the anime season and year
+        if None in metadata["season"].values():
+            new_location = season_dir / 'unsorted'
+        else:
+            new_location = season_dir / (f'{metadata["season"]["year"]}/{metadata["season"]["season"].lower()}')
+        print(new_location)
+
+        new_location.mkdir(exist_ok=True, parents=True)
+
+        os.rename(anime, new_location / anime.name)
+        
         print("----------------------")
+        return
 
 
 if __name__ == '__main__':
-    run()
+    sort_seasons()
